faa51e86b5f991206b560ec07047076e6052e2a3
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-01-16 22:54:38 -0200

    Playing with travis

---
+++ scripts/docker_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ev
+
+echo "TRAVIS_EVENT_TYPE ${TRAVIS_EVENT_TYPE}"
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]
+then
+  echo docker build -t apinf/platform:$DOCKER_TAG .
+  echo docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  echo docker push apinf/platform:$DOCKER_TAG
+else
+  echo "From a PULL REQUEST - what would be best"
+  echo "docker push mauriciovieira/platform:${TRAVIS_PULL_REQUEST_BRANCH} TRAVIS_PULL_REQUEST_BRANCH"
+  echo "docker push mauriciovieira/platform:${TRAVIS_PULL_REQUEST_SHA} TRAVIS_PULL_REQUEST_SHA"
+fi
+
+  echo "docker push mauriciovieira/platform:${TRAVIS_BRANCH} TRAVIS_BRANCH"
+  echo "docker push mauriciovieira/platform:${TRAVIS_BUILD_ID} TRAVIS_BUILD_ID"
+  echo "docker push mauriciovieira/platform:${TRAVIS_BUILD_NUMBER} TRAVIS_BUILD_NUMBER"
